01f0880aa0ae92b63d0dd4f6224c131070606c24
Simtoon Software <simon.slamka@adlerka.sk>
2019-05-23 09:13:25 +0200

    disabled process kill on 'AboutActivity' exit

--- app/src/main/java/com/simtoonsoftware/taxthegame/AboutActivity.java
+++ app/src/main/java/com/simtoonsoftware/taxthegame/AboutActivity.java
@@ -9,13 +9,13 @@ import com.google.android.gms.ads.MobileAds;
 
 public class AboutActivity extends AppCompatActivity {
 
-    @Override
+    /*@Override
     protected void onPause() {
         super.onPause();
         System.out.println("Exiting AboutActivity ..."); //printing to console
         android.os.Process.killProcess(android.os.Process.myPid());
         finish();
-    }
+    }*/
 
     private AdView RandomBannerAd;
 
