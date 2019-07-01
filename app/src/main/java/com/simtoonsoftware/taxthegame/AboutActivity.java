55d1600792d6e0ff69e6b267e2cbc3af2ae015e7
simonSlamkaSMD <SMDexec000@protonmail.ch>
2019-07-01 17:21:25 +0200

    added a button linking to our Privacy Policy to the About activity;

--- app/src/main/java/com/simtoonsoftware/taxthegame/AboutActivity.java
+++ app/src/main/java/com/simtoonsoftware/taxthegame/AboutActivity.java
@@ -1,6 +1,11 @@
 package com.simtoonsoftware.taxthegame;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
@@ -25,10 +30,22 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        final Button btn_PrivacyPolicy = findViewById(R.id.btn_privacyPolicy);
+
         MobileAds.initialize(this, "ca-app-pub-9086446979210331~8508547502");
 
         RandomBannerAd = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         RandomBannerAd.loadAd(adRequest);
+
+
+        btn_PrivacyPolicy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.iubenda.com/privacy-policy/12620960/full-legal")));
+            }
+
+                                             });
+
     }
 }
